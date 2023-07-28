d370be27d28664eac11ef29729511b9ca6e69fed
Benjamin  Cockburn <ben@Benjamins-MacBook-Air-2.local>
2023-07-28 11:41:00 +0930

    ekkle

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include Human.h
+#include Player.h
+#include Referee.h
+#include Computer.h
+
+int main()
+{
+    Referee referee;
+
+    Human player1;
+
+    Computer player2;
+}
